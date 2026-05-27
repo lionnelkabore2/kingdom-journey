ca3b180c444978558e55ead5f33b7640fdf0d94a
Lionnel <lionnelkabore2@gmail.com>
2026-05-27 07:35:07 +0000

    Fix redirect

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 
 export default function Home() {
-  const router = useRouter();
   useEffect(() => {
     window.location.href = '/index.html';
   }, []);
